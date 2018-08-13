c835ca69658a054685228c35ad9bd20a9475182c
Your Name <you@example.com>
2018-08-13 17:03:16 +0800

    added cat like function

---
+++ cats.js
@@ -0,0 +1,11 @@
+//this program will tell us about our cats
+
+let cats = [
+    {name: 'Tabby', likes: 'catnip'},
+    {name: 'Bob Ross', likes: 'paint'},
+    {name: 'Chaos', likes: 'knocking over plants'}
+]
+
+for (let i=0; i< cats.length; i++) {
+    console.log(`${cats[i].name} likes ${cats[i].likes}`);
+}
